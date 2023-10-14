a1ab7ae143e95fb9b2c231c057c02d9eeaa4653a
Nandan1807 <146802591+Nandan1807@users.noreply.github.com>
2023-10-14 15:07:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const mongoDb = require("./db");
+const cors = require("cors");
+
 
 mongoDb();
 
-app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  )
-  next();
-})
+const corsOpts={
+  origin:'*',
+  methods:[
+    'GET',
+    'POST'
+  ],
+  allowedHeaders:[
+    'Content-Type',
+  ]
+};
+
+app.use(cors(corsOpts));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
